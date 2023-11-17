cff221368df29838d4d0f5a21d9a2f07ffded73e
csm <csm009177@gmail.com>
2023-11-17 17:15:28 +0900

    want make hash

--- module/creMenu.js
+++ module/creMenu.js
@@ -1,12 +1,14 @@
-function creMenu(fatag, menu){
-  const head = document.getElementById(fatag)
+function creMenu(fatagid, menu){
+  
+  const fatag = document.getElementById(fatagid)
   const creHeadElem = document.createElement('div')
   creHeadElem.setAttribute("id", menu);
   creHeadElem.setAttribute("type", "button");
   creHeadElem.style.width = "25vw"
   const textNode = document.createTextNode(menu);
   creHeadElem.appendChild(textNode);
-  head.appendChild(creHeadElem)
+  fatag.appendChild(creHeadElem)
+  
 }
 
 module.exports = creMenu;

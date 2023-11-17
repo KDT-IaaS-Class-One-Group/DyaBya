743558ebf1689ebfcafbf8d7192377c4ecce3c38
kimwoojin123 <kimwoojin1903@gmail.com>
2023-11-17 14:09:51 +0900

    서버 구축

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello-dev')
+const express = require("express");
+const app = express();
+const router = require("./routes");
+
+app.use(express.static("public"));
+
+app.use("/", router);
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000/");
+});

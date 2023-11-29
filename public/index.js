c47deffcb48c9e0ff41c104cd4078707ea232426
csm <csm009177@gmail.com>
2023-11-29 16:27:42 +0900

    try to dipart component

---
+++ public/index.js
@@ -0,0 +1,49 @@
+    // init createElement
+    function createElement(type, props, ...children) {
+      return { type, props, children};
+    }
+    // comoponent
+    function component(stateData) {
+      const menuItems = [];
+      for (let i=0; i<stateData.length; i++){
+        const item = stateData[i];
+        const menuItem = createElement('li', {style:'margin:auto; list-style:none;'}, createElement('a', {href:item.hash}, item.text));
+        menuItems.push(menuItem);
+      }
+
+      const menu = createElement('ul', {
+        style:'width: 100vw; height: 10vh;background-color: gray; display: flex;  flex-direction: row; text-align: center; justify-content: center;      align-items: center;'}, ...menuItems);
+
+        
+      const content = createElement('div', {}, '');
+      return createElement('div', {}, menu, content);
+    }
+
+    function render(virtualDom) {
+      if(typeof virtualDom === 'string') {
+        return document.createTextNode(virtualDom);
+      }
+      const element = document.createElement(virtualDom.type);
+      if(virtualDom.props) {
+        for( const [key,value] of Object.entries(virtualDom.props)) {
+          element.setAttribute(key,value);
+        }
+      }
+      for(let i=0; i<virtualDom.children.length; i++) {
+        const child = virtualDom.children[i];
+        element.appendChild(render(child));
+      }
+      return element;
+    }
+
+    const stateData = [
+      { hash: '#home', text: 'Home', name: "Home" },
+      { hash: '#team', text: 'Team', name: "Team" },
+      { hash: '#member', text: 'Member', name: "Member" },
+      { hash: '#purpose', text: 'Purpose', name: "Purpose" },
+    ];
+
+
+  const virtualDom = component(stateData);
+  const container = document.getElementById('head');
+  container.appendChild(render(virtualDom));

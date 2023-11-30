ebfa2d5160603a2f6ccd83e8bbe4d34f08d7332a
csm <csm009177@gmail.com>
2023-11-30 17:09:58 +0900

    good switch good

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@
     export function createElement(type, props, ...children) {
       return { type, props, children};
     }
-    // comoponent
+    // head comoponent
     export function component(stateData) {
       const menuItems = [];
       for (let i=0; i<stateData.length; i++){
@@ -17,6 +17,25 @@
         
       const content = createElement('div', {}, '');
       return createElement('div', {}, menu, content);
+    }
+    
+    export function component2(elementNode, attributes, children) {
+      let elementStr = `<${elementNode}`;
+      for (let key in attributes) {
+        elementStr += `${key} = "${attributes[key]}"`;
+      }
+      elementStr += '>';
+      if (children) {
+        children.forEach((child)=> {
+          if (typeof child === 'string') {
+            elementStr += child;
+          } else {
+            elementStr += component(child.elementNode, child.attributes, child.children);
+          }
+        });
+      }
+      elementStr += `</${elementNode}>`;
+      return elementStr;
     }
 
     export function render(virtualDom) {

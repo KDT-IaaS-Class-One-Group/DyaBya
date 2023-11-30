f921ea73370266cbf24f929cf0f78ee7ba43afde
JungYoungSick <junfas1216@gmail.com>
2023-11-30 16:33:13 +0900

    모듈 폴더 및 파일 생성. 분류작업

--- routes.js
+++ routes.js
@@ -1,10 +1,28 @@
 import express from "express";
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import path from "path";  // 이 부분을 추가합니다.
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "public", "index3-1.html"));
 });
 
 export default router;
+
+
+// import express from "express";
+// import path from "path";
+
+// const router = express.Router();
+
+// router.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public/index3-1.html"));
+// });
+
+// export default router;

5363658fd75b3f893a16d1edde2942545b00e974
csm <csm009177@gmail.com>
2023-12-01 11:06:32 +0900

    #home prompt

--- routes.js
+++ routes.js
@@ -6,5 +6,9 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+// <form id="memo-Form" method="POST">
+router.post("/", (req, res)=> {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+})
 
 export default router;

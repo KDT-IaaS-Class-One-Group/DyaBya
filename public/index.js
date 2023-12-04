24f261bedd89f0d66be838dd9fb8af4e559492fc
csm <csm009177@gmail.com>
2023-12-04 17:20:24 +0900

    make new comp style

--- public/index.js
+++ public/index.js
@@ -7,15 +7,12 @@
       const menuItems = [];
       for (let i=0; i<stateData.length; i++){
         const item = stateData[i];
-        const menuItem = createElement('li', {style:'margin:auto; list-style:none;'}, createElement('a', {href:item.hash}, item.text));
+        const menuItem = createElement('li', {style:"list-style: none;"}, createElement('a', {href:item.hash}, item.text));
         menuItems.push(menuItem);
       }
-
-      const menu = createElement('ul', {
-        style:'width: 100vw; height: 10vh;background-color: #00cc33; display: flex;  flex-direction: row; text-align: center; justify-content: center;      align-items: center;'}, ...menuItems);
-
-      const content = createElement('div', {}, '');  // 
-      return createElement('div', {}, menu, content); // retrun cont include menu and content 
+      return createElement('div', { 
+        style:"display: flex; text-align: center; flex-direction: column; align-items: center;"}, 
+        ...menuItems);
     }
 
     export function component2(elementNode, attributes, children) {

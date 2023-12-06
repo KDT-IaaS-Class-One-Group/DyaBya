892e2432fd63b01400209ebe6ef3337f6935da7c
승희 <bangseunghee1108@gmail.com>
2023-12-06 17:27:27 +0900

    style colors 수정 테스트

--- public/style.js
+++ public/style.js
@@ -3,4 +3,8 @@ export function style(data) {
   // 동적으로 스타일 적용
   const head = document.getElementById("head");
   head.style.backgroundColor = colors.head;
+  
+  // const home = document.getElementById("home");
+  head.style.backgroundColor = colors.home.title;
+  
 }

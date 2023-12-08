62a10789fb799c7089ce89313276d8ba343f60e3
csm <csm009177@gmail.com>
2023-12-08 12:22:05 +0900

    comment about render

--- public/module/component.js
+++ public/module/component.js
@@ -17,38 +17,40 @@ export function component(stateData) {
   return createElement("div", {}, menu); // result cont is including menu
 }
 
-export function component2(elementNode, attributes, children) {
-  let elementStr = `<${elementNode}`;
-  for (let key in attributes) {
-    elementStr += ` ${key} = "${attributes[key]}"`;
+
+// component2 result is elementStr
+export function component2(elementNode, attributes, children) { 
+  let elementStr = `<${elementNode}`; // start frontelementStr  and put element name                           
+  for (let key in attributes) {       // in all attributes 
+    elementStr += ` ${key} = "${attributes[key]}"`; // matching key and attributes
   }
-  elementStr += ">";
-  if (children) {
-    children.forEach((child) => {
+  elementStr += ">"; // end frontelementStr 
+  if (children) {    // if children exist add chilren
+    children.forEach((child) => { // 
       if (typeof child === "string") {
         elementStr += child;
-      } else {
+      } else {  // 
         elementStr += component(child.elementNode, child.attributes, child.children);
       }
     });
   }
-  elementStr += `</${elementNode}>`;
-  return elementStr;
+  elementStr += `</${elementNode}>`; // end rearelementStr
+  return elementStr;                 // done
 }
 
-export function render(virtualDom) {
-  if (typeof virtualDom === "string") {
-    return document.createTextNode(virtualDom);
+export function render(virtualDom) {  
+  if (typeof virtualDom === "string") {  // type check 
+    return document.createTextNode(virtualDom); // 태그가 이 텍스트노드를 감싸도록 하는법
   }
-  const element = document.createElement(virtualDom.type);
-  if (virtualDom.props) {
-    for (const [key, value] of Object.entries(virtualDom.props)) {
-      element.setAttribute(key, value);
+  const element = document.createElement(virtualDom.type); // declare element is type of dom
+  if (virtualDom.props) {   // if props is exist
+    for (const [key, value] of Object.entries(virtualDom.props)) { 
+      element.setAttribute(key, value); // search all props and matching
     }
   }
-  for (let i = 0; i < virtualDom.children.length; i++) {
-    const child = virtualDom.children[i];
-    element.appendChild(render(child));
+  for (let i = 0; i < virtualDom.children.length; i++) { 
+    const child = virtualDom.children[i]; // declare child is each children
+    element.appendChild(render(child));   // render child and append to element
   }
-  return element;
+  return element; 
 }

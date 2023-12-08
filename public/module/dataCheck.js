51e73737f10e07efabbd3348ce15ec16d306f616
JungYoungSick <junfas1216@gmail.com>
2023-12-08 12:32:22 +0900

    dataCheck.js 주석 완료

--- public/module/dataCheck.js
+++ public/module/dataCheck.js
@@ -1,4 +1,5 @@
-const memberData = {
+//? 변수 데이터를 이용한 데이터 호출 및 유효성 검사.
+const memberData = { // memberData라는 변수에 성씨와 깃허브 주소와 취미, 각오를 객체 안 데이터로 작성.
   kim: {
     url: "https://github.com/kimwoojin123/",
     hobby: ["음악감상", "자전거 타기"],
@@ -21,15 +22,16 @@ const memberData = {
   },
 };
 
-export function dataCheck(firstName) {
-  const url = memberData[firstName].url;
-  const hobby = memberData[firstName].hobby;
-  const urlCheck = url.includes("https://github.com/");
-  const hobbyCheck = Object.keys(hobby).length !== 0;
+//? 데이터 유효성 검사를 통한 해당 데이터 불러오기.
+export function dataCheck(firstName) { // 모듈화 진행. dataCheck함수에 매개변수 firstName을 담음.
+  const url = memberData[firstName].url; // url 변수 선언.(변수 memberData에 해당하는 성씨(firstName)의 url 호출)
+  const hobby = memberData[firstName].hobby; // hobby 변수 선언. (변수 memberData에 해당하는 성씨(firstName)의 hobby 호출)
+  const urlCheck = url.includes("https://github.com/"); // urlCheck 변수 선언. (url변수에서 "https://github.com/"데이터를 확인)
+  const hobbyCheck = Object.keys(hobby).length !== 0; // hobbyCheck 변수 선언. (객체의 key값 중 hobby의 데이터를 찾아 길이 값중 해당하는 값을 확인)
 
-  if (urlCheck && hobbyCheck) {
-    return `Url : ${memberData[firstName].url}<br> Hobby : ${memberData[firstName].hobby}<br> Resolution : ${memberData[firstName].resolution}`;
+  if (urlCheck && hobbyCheck) { // 조건문 url데이터를 확인하고 hobbyCheck가 참일경우. 
+    return `Url : ${memberData[firstName].url}<br> Hobby : ${memberData[firstName].hobby}<br> Resolution : ${memberData[firstName].resolution}`; // memberData에서 firstName의 url을 꺼내오는 방식으로 모든 데이터도 동일방식을 사용하여 정보를 출력한다.
   } else {
-    return `적절한 데이터를 입력해주세요`;
+    return `적절한 데이터를 입력해주세요`; // 해당하는 데이터가 없을 경우 `적절한 데이터를 입력해주세요` 라고 데이터를 출력시킨다.
   }
 }
